10ce4c5516f76bc0dc65f8402d5616845e4850ff
laskor <al.moksud@gmail.com>
2022-03-11 19:54:06 -0500

    delete

--- properties.js
+++ properties.js
@@ -1,11 +1,27 @@
 // Keys, values, entries, delete, seal, freeze
 
-const bottle = { color: 'yellow', price: 50, hold: 'water', isClean: true };
+const bottle = { color: 'yellow',
+                 price: 50, 
+                 hold: 'water', 
+                 isClean: true };
 
 //1. getting all propertyies name
+
 const Keys = Object.keys(bottle);
 // console.log(Keys);
 
 // 2. getting all values
+
 const values = Object.values(bottle);
-console.log(values);
+// console.log(values);
+
+// 3. key, value pairs
+
+const pairs = Object.entries(bottle);
+// console.log(pairs);
+
+// 4.delete from obj
+delete bottle.isClean;
+// console.log(bottle);
+
+// 5.seal (can't delete property but can change )
