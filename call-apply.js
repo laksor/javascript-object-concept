21e45ed695e5bbcb5396373e9c1ea3c68a5d98c0
laskor <al.moksud@gmail.com>
2022-03-12 18:07:21 -0500

    using call

---
+++ call-apply.js
@@ -0,0 +1,27 @@
+
+// call and apply
+
+const Kibria = {
+    id: 101,
+    money: 5000,
+    name: 'Rj Kibria',
+    treatDey: function(expense, tip, tax){
+        this.money = this.money - expense - tip - tax;
+        console.log(this);
+        return this.money;
+    }
+}
+
+const heroB = {
+    id: 102,
+    money: 6000,
+    name: 'Hero Balam'
+}
+// Kibria.treatDey(100);
+// using bind 
+// const heroTreatDey = Kibria.treatDey.bind(heroB);
+// heroTreatDey(500);
+
+// using call
+
+Kibria.treatDey.call(heroB, 500, 100, 50);
