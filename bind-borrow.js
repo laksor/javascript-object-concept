7d495ce6e32313c8fd1c807a31d3923c27f04a33
laskor <al.moksud@gmail.com>
2022-03-12 18:01:40 -0500

    bind

--- bind-borrow.js
+++ bind-borrow.js
@@ -7,6 +7,18 @@ const Kibria = {
     name: 'Rj Kibria',
     treatDey: function(expense){
         this.money = this.money - expense;
+        console.log(this);
         return this.money;
     }
 }
+
+const heroB = {
+    id: 102,
+    money: 6000,
+    name: 'Hero Balam'
+}
+Kibria.treatDey(100);
+
+// using bind 
+const heroTreatDey = Kibria.treatDey.bind(heroB);
+heroTreatDey(500);
